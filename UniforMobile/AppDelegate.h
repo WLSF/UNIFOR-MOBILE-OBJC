145603dd82a787092b71604edcc1924106d8ed72
aluno-r17 <alunor-17@Mac-mini-de-alunor17-49.local>
2014-08-05 14:15:54 -0300

    Initial Commit

---
+++ UniforMobile/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UniforMobile
+//
+//  Created by aluno-r17 on 05/08/14.
+//  Copyright (c) 2014 aluno-r17. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UniforMobile/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UniforMobile
+//
+//  Created by aluno-r17 on 05/08/14.
+//  Copyright (c) 2014 aluno-r17. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
